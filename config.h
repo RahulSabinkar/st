1d54161a093cdd72e0a119d26287c6009e18fffd
Rahul Sabinkar <rahulsabinkar4u@gmail.com>
2020-11-07 17:34:42 +0000

    changed font to inconsolata-g

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=19:antialias=true:autohint=true";
+static char *font = "Inconsolata\-g:style=g:pixelsize=19:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=19:antialias=true:autohint=true" };
 static int borderpx = 2;
 
