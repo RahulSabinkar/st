3bd206729d14a92ed59728be94a44c918dddb355
Rahul Sabinkar <rahulsabinkar4u@gmail.com>
2020-12-08 22:07:21 +0000

    changed font to JetBrainMono

--- config.h
+++ config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata-g:style=g:pixelsize=19:antialias=true:autohint=true";
+static char *font = "JetBrainsMono:pixelsize=26:antialias=true:autohint=true";
+//static char *font = "Inconsolata-g:style=g:pixelsize=19:antialias=true:autohint=true";
 //static char *font = "Source Code Pro:pixelsize=19:antialias=true:autohint=true";
 //static char *font = "MesloLGS NF:style=Regular:pixelsize=19:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=20:antialias=true:autohint=true" };
